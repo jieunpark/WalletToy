83a0cf2b8af8ae036297bafeea8ce373fb3880d1
jepark <jepark@mhand.net>
2021-10-28 17:25:32 +0900

    :add 환경세팅
    coroutine, hilt

--- app/src/main/java/com/example/wallettoy/MainActivity.kt
+++ app/src/main/java/com/example/wallettoy/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.wallettoy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.wallettoy.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
     }
 }
