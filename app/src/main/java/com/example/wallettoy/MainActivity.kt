9505f4a71cb3a4b3418d8edfce54a2104a16b9de
jepark <jepark@mhand.net>
2021-10-29 18:34:29 +0900

    :feat 클레이전송 진행중

--- app/src/main/java/com/example/wallettoy/MainActivity.kt
+++ app/src/main/java/com/example/wallettoy/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.wallettoy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.wallettoy.common.Config.ADDRESS
+import com.example.wallettoy.common.Config.ADDRESS2
+import com.example.wallettoy.common.Config.KEY
+import com.klaytn.caver.Caver
+import com.klaytn.caver.transaction.type.ValueTransfer
+import java.io.File
+import java.math.BigInteger
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sendingKLAY()
+    }
+
+    private fun sendingKLAY() {
+
+        val caver = Caver("BAOBAB_URL")
+
+        // caver.wallet에 키링 추가
+        val keyring = caver.wallet.newKeyring(ADDRESS, KEY)
+//        caver.wallet.add(keyring)
+
+        // 자산 이동 트랜잭션 생성
+        val valueTransfer: ValueTransfer = ValueTransfer.Builder()
+            .setFrom(keyring.address)
+            .setTo(ADDRESS2)
+            .setNonce("")
+            .setChainId("")
+            .setValue(BigInteger.valueOf(1))
+            .setGasPrice("0.1")
+            .setGas(BigInteger.valueOf(30000))
+            .build()
+
+        // caver.waller.sign으로 트랜잭션 서명
+        caver.wallet.sign(keyring.address, valueTransfer)
+        val rlpEncoded = valueTransfer.rlpEncoding
+        Log.w("TEST", ">>>>> RLP ENCODING $rlpEncoded")
+
     }
 }
---
+++ app/src/main/java/com/example/wallettoy/common/Config.kt
@@ -0,0 +1,11 @@
+package com.example.wallettoy.common
+
+object Config {
+
+    // 계정 1
+    const val ADDRESS = "0x8895795a932dc64304eb98efd7ca200a4e74b1d9"
+    const val KEY = "0x579dbc397669f8e54f879b79d891b32a55b991f05d6fc076bfd1e5aa7aa99c30"
+
+    // 계정2
+    const val ADDRESS2 = "0x8895795a932dc64304eb98efd7ca200a4e74b1d9"
+}
