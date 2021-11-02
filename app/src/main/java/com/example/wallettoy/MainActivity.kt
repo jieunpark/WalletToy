641780772cb2ea988891a3359156d3f5e3057a5a
jepark <jepark@mhand.net>
2021-11-02 15:26:11 +0900

    :feat 자산 이동 트랙잭션 이동 수정

--- app/src/main/java/com/example/wallettoy/MainActivity.kt
+++ app/src/main/java/com/example/wallettoy/MainActivity.kt
@@ -6,7 +6,9 @@ import android.util.Log
 import com.example.wallettoy.common.Config.ADDRESS
 import com.example.wallettoy.common.Config.ADDRESS2
 import com.example.wallettoy.common.Config.KEY
+import com.example.wallettoy.common.Config.URL_BAOBAB
 import com.klaytn.caver.Caver
+import com.klaytn.caver.transaction.TxPropertyBuilder
 import com.klaytn.caver.transaction.type.ValueTransfer
 import java.io.File
 import java.math.BigInteger
@@ -21,22 +23,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun sendingKLAY() {
 
-        val caver = Caver("BAOBAB_URL")
+        val caver = Caver(URL_BAOBAB)
 
         // caver.wallet에 키링 추가
         val keyring = caver.wallet.newKeyring(ADDRESS, KEY)
-//        caver.wallet.add(keyring)
+
 
         // 자산 이동 트랜잭션 생성
-        val valueTransfer: ValueTransfer = ValueTransfer.Builder()
-            .setFrom(keyring.address)
-            .setTo(ADDRESS2)
-            .setNonce("")
-            .setChainId("")
-            .setValue(BigInteger.valueOf(1))
-            .setGasPrice("0.1")
-            .setGas(BigInteger.valueOf(30000))
-            .build()
+        val valueTransfer = caver.transaction.valueTransfer.create(
+            TxPropertyBuilder.valueTransfer()
+                .setFrom(keyring.address)
+                .setTo(ADDRESS2)
+                .setValue(BigInteger.valueOf(1))
+                .setGas(BigInteger.valueOf(30000))
+        )
 
         // caver.waller.sign으로 트랜잭션 서명
         caver.wallet.sign(keyring.address, valueTransfer)
--- app/src/main/java/com/example/wallettoy/common/Config.kt
+++ app/src/main/java/com/example/wallettoy/common/Config.kt
@@ -2,6 +2,8 @@ package com.example.wallettoy.common
 
 object Config {
 
+    const val URL_BAOBAB = "https://api.baobab.klaytn.net:8651"
+
     // 계정 1
     const val ADDRESS = "0x8895795a932dc64304eb98efd7ca200a4e74b1d9"
     const val KEY = "0x579dbc397669f8e54f879b79d891b32a55b991f05d6fc076bfd1e5aa7aa99c30"
